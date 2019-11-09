3d984999a39e1c20593c2812ce6c64d7a8587f87
jlinzhou <772945866@qq.com>
2019-11-09 14:06:36 +0800

    '完成了登录'

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,13 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    'spaced-comment': 'off',
+    'no-tabs': 'off',
+    'indent': 'off',
+    'space-before-function-paren': 'off',
+    'no-trailing-spaces': 'off',
+    'eol-last': 'off',
+    // 'import/no-duplicates': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<!--  -->
+<template>
+	<div>
+		<el-button type="info" @click="logout">退出</el-button>
+	</div>
+</template>
+
+<script>
+export default {
+	components: {},
+	methods: {
+		logout() {
+			window.sessionStorage.clear()
+			this.$router.push('/login')
+		}
+	},
+	data() {
+		return {}
+	}
+}
+</script>
+ 
+<style scoped lang = "less">
+</style>
